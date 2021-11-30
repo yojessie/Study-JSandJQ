cd0076d4d86b3bbfff5ffc3657270028c84199c2
yojessie <sprout.jessie@gmail.com>
2021-12-01 00:16:22 +0900

    ♻️  Add one more button and refactor it using some parameter

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const alert = document.querySelector('.alert');
+const span = document.querySelector('.alert span');
 const alertButton = document.querySelector('.alert button');
-const button = document.querySelector('.make-alert');
+const button1 = document.querySelector('.make-alert.first');
+const button2 = document.querySelector('.make-alert.second');
 
-function makeAlert() {
-    alert.classList.remove('hidden');
-    button.classList.add('hidden');
+function handleAlert(property1, message, property2) {
+    alert.style.display = property1;
+    button1.style.display = property2;
+    button2.style.display = property2;
+    span.innerHTML = message;
 }
 
-function closeAlert() {
-    alert.classList.add('hidden');
-    button.classList.remove('hidden');
-}
+button1.addEventListener('click', function(){handleAlert('flex', '아이디를 입력하세요', 'none')});
+button2.addEventListener('click', function(){handleAlert('flex', '비밀번호를 입력하세요', 'none')});
+alertButton.addEventListener('click', function(){handleAlert('none', '', 'inline-block')});
 
-button.addEventListener('click', makeAlert);
-alertButton.addEventListener('click', closeAlert);

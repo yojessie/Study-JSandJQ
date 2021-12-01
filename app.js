9181af14bbb24df9429a64400dde1508f25593d2
yojessie <sprout.jessie@gmail.com>
2021-12-01 14:36:34 +0900

    ♻️  Refactor by using JQuery

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const alert = document.querySelector('.alert');
-const span = document.querySelector('.alert span');
-const alertButton = document.querySelector('.alert button');
-const button1 = document.querySelector('.make-alert.first');
-const button2 = document.querySelector('.make-alert.second');
+const alert = $('.alert');
+const span = $('.alert span');
+const alertButton = $('.alert button');
+const button1 = $('.make-alert.first');
+const button2 = $('.make-alert.second');
 
 function handleAlert(property1, message, property2) {
-    alert.style.display = property1;
-    button1.style.display = property2;
-    button2.style.display = property2;
-    span.innerHTML = message;
+    alert.css('display', property1);
+    button1.css('display', property2);
+    button2.css('display', property2);
+    span.html(message);
 }
 
-button1.addEventListener('click', function(){handleAlert('flex', '아이디를 입력하세요', 'none')});
-button2.addEventListener('click', function(){handleAlert('flex', '비밀번호를 입력하세요', 'none')});
-alertButton.addEventListener('click', function(){handleAlert('none', '', 'inline-block')});
-
+button1.on('click', function(){handleAlert('flex', '아이디를 입력하세요', 'none')});
+button2.on('click', function(){handleAlert('flex', '비밀번호를 입력하세요', 'none')});
+alertButton.on('click', function(){handleAlert('none', '', 'inline-block')});

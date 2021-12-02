9b2409da1ac37ef8bd486a429e81e0487aae5b61
yojessie <sprout.jessie@gmail.com>
2021-12-02 14:13:37 +0900

    ✨  Make a modal and a nav-menu by using Bootstrap

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const alert = $('.alert');
-const span = $('.alert span');
-const alertButton = $('.alert button');
-const button1 = $('.make-alert.first');
-const button2 = $('.make-alert.second');
+// Handle more Modal
+const moreButton = $('.btn');
+const closeButton = $('.close-button');
+const textModal = $('.more-modal');
 
-function handleAlert(property1, message, property2) {
-    alert.css('display', property1);
-    button1.css('display', property2);
-    button2.css('display', property2);
-    span.html(message);
-}
 
-button1.on('click', function(){handleAlert('flex', '아이디를 입력하세요', 'none')});
-button2.on('click', function(){handleAlert('flex', '비밀번호를 입력하세요', 'none')});
-alertButton.on('click', function(){handleAlert('none', '', 'inline-block')});
+moreButton.on('click', function(){
+    textModal.toggle();
+})
+
+closeButton.on('click', function(){
+    textModal.toggle();
+})
+
+
+// Handle GNB menu
+const menuButton = $('.nav-button');
+const menuList = $('.nav-sub');
+
+menuButton.click(function(){
+    menuList.slideToggle();
+})

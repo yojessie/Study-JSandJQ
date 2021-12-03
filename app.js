5fb129b80df4e2e3ef30986fb716c1fe833518ee
yojessie <sprout.jessie@gmail.com>
2021-12-03 16:17:00 +0900

    🚧  Improve login modal and make 369 Games

--- app.js
+++ app.js
@@ -16,11 +16,19 @@ closeButton.on('click', function(){
 // Handle login submit
 const loginForm = $('.login-form');
 const emailInput = $('.email-input');
+const passwordInput = $('.password-input');
+const warningEmail = $('.warning.email');
+const warningPassword = $('.warning.password');
 
 loginForm.on('submit', function(event){
     if (emailInput.val() == '') {
         event.preventDefault();
-        alert('Please enter your Email');
+        warningEmail.show();
+    } 
+    
+    if (passwordInput.val() == '') {
+        event.preventDefault();
+        warningPassword.show();
     }
 })
 
@@ -32,3 +40,29 @@ const menuList = $('.nav-sub');
 menuButton.click(function(){
     menuList.slideToggle();
 })
+
+
+// 369 Game
+function game369(number) {
+    if (number % 9 == 0) {
+        console.log('clap clap');
+    } else if (number % 3 == 0) {
+        console.log('clap');
+    } else {
+        console.log('pass'); 
+    }
+}
+
+game369(18);
+
+
+// Real 369 Game
+function real369game(number) {
+    if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+        console.log('clap');
+    } else {
+        console.log('pass');
+    }
+}
+
+real369game(19);

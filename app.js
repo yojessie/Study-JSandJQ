2772810fd4cd3cb67eaec42d69cc912598bbfd9f
yojessie <sprout.jessie@gmail.com>
2021-12-05 00:18:18 +0900

    🚧  Make left menu and add animation

--- app.js
+++ app.js
@@ -42,69 +42,15 @@ menuButton.click(function(){
 })
 
 
-// 369 Game
-function game369(number) {
-    if (number % 9 == 0) {
-        console.log('clap clap');
-    } else if (number % 3 == 0) {
-        console.log('clap');
-    } else {
-        console.log('pass'); 
-    }
-}
+// Handle left menu
+const leftMenuButton = $('.left-menu-button');
+const leftMenu = $('.left-menu');
+const leftClose = $('.left-menu-close');
 
-game369(18);
+leftMenuButton.click(function(){
+    leftMenu.animate({marginLeft : '0'}, 500);
+});
 
-
-// Real 369 Game
-function real369game(number) {
-    if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
-        console.log('clap');
-    } else {
-        console.log('pass');
-    }
-}
-
-real369game(19);
-
-
-// 이자율 계산하기
-// function depositAfter(deposit) {
-//     if (deposit <= 50000) {
-//         console.log(deposit * 1.15 * 1.15);
-//     } else {
-//         console.log(deposit * 1.2 * 1.2);
-//     }
-// }
-// depositAfter(60000);
-
-let deposit = 60000;
-let depositAfter = 0;
-
-if (deposit <= 50000) {
-    depositAfter = deposit * 1.15 * 1.15;
-} else {
-    depositAfter = deposit * 1.2 * 1.2;
-}
-
-console.log(depositAfter);
-
-
-// 최대 커피 리필양 계산하기
-// let coffee = 360;
-// let firstRefill = coffee * 2 / 3;
-// let secondRefill = firstRefill * 2 / 3;
-// let coffeeDrank = coffee + firstRefill + secondRefill;
-// console.log(coffeeDrank + 'ml');
-
-let coffee = 360;
-let coffeeDrank = 0;
-
-let refill = coffee * 2 / 3;
-coffeeDrank = coffee + refill;
-
-refill = coffee * 2 / 3;
-coffeeDrank = coffee + refill;
-
-console.log(coffeeDrank + 'ml');
-
+leftClose.click(function(){
+    leftMenu.animate({marginLeft : '-240px'}, 500);
+})
